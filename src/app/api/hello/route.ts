c0f4bb5c7e1d5ba09d6fca6202f969f1abb9661a
unknown <William@WILLIAM.digiworld.local>
2023-11-23 08:39:05 +0700

    first commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,6 @@
+export async function GET() {
+  return Response.json({ data: "hello world" });
+}
+export async function POST() {
+  return Response.json({ data: "yo whats up" });
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import LoginForm from "@/components/LoginForm";
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <LoginForm />
+    </main>
+  );
+}
